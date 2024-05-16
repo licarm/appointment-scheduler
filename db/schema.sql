d7420503ea2acd797ba86993212474349ade29be
carmen <carmen@carmens-MacBook-Pro.local>
2024-05-17 00:34:38 +0800

    change id to varchar

--- db/schema.sql
+++ db/schema.sql
@@ -2,13 +2,13 @@ CREATE SCHEMA myschema;
 
 CREATE TABLE myschema.user (
   "name" varchar NOT NULL,
-  id int NOT NULL PRIMARY KEY,
+  id varchar NOT NULL PRIMARY KEY,
   phone_number varchar(15) NOT NULL,
   user_type varchar not null
 );
 
 CREATE TABLE myschema.opening (
-  user_coach_id INT,
+  user_coach_id varchar,
   time timestamp with time zone NOT NULL,
   CONSTRAINT fk_opening_user FOREIGN KEY (user_coach_id) REFERENCES myschema.user(id),
   UNIQUE (user_coach_id, time)
@@ -16,17 +16,17 @@ CREATE TABLE myschema.opening (
 
 CREATE TABLE myschema.call (
   id int PRIMARY KEY,
-  user_coach_id INT NOT NULL,
-  user_student_id INT NOT NULL,
+  user_coach_id varchar NOT NULL,
+  user_student_id varchar NOT NULL,
   score INT,
   notes varchar,
   time timestamp with time zone NOT NULL,
   FOREIGN KEY (user_coach_id) REFERENCES myschema.user(id)
 );
 
-INSERT INTO myschema.user VALUES ('Alice Coach', 1, '7789991111', 'coach');
-INSERT INTO myschema.user VALUES ('Bob Coach', 2, '7789992222', 'coach');
-INSERT INTO myschema.user VALUES ('Carol Student', 3, '7789993333', 'student');
-INSERT INTO myschema.user VALUES ('David Student', 4, '7789994444', 'student');
+INSERT INTO myschema.user VALUES ('Alice Coach', '1', '7789991111', 'coach');
+INSERT INTO myschema.user VALUES ('Bob Coach', '2', '7789992222', 'coach');
+INSERT INTO myschema.user VALUES ('Carol Student', '3', '7789993333', 'student');
+INSERT INTO myschema.user VALUES ('David Student', '4', '7789994444', 'student');
 
 
