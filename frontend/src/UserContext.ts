93b9a815f1b124ef5f7a39c77c4dfc323edcb416
carmen <carmen@carmens-MacBook-Pro.local>
2024-05-17 00:58:02 +0800

    remove files

--- frontend/src/UserContext.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { User } from "src/UserTypes";
-
-export const UserContext = React.createContext<User | null>(null);
--- frontend/src/UserTypes.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-  name: string,
-  phoneNumber: string,
-  id: number,
-  userType: string,
-}
