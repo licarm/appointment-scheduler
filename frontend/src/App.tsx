6bbc50064c30a604e3f30a7b6a082a4d2b46148e
carmen <carmen@carmens-MacBook-Pro.local>
2024-05-17 00:57:47 +0800

    user context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,40 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import UserSelector from 'src/common-components/UserSelector';
+import CoachPage from 'src/pages/Coach';
+import StudentPage from 'src/pages/Student';
+import { UserContext } from 'src/utils/UserContext';
+import { User } from 'src/Types';
 
 
 function App() {
   const path = window.location.pathname;
-  // const [errors, setErrors] = useState('');
-
-
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
 
   return (
     <div className="App">
-        <UserSelector />
-      <header className="App-header">
-        {/* {path === '/coach'
-          ? <CoachPage />
-          : path === '/student'
-            ? <StudentPage />
-            : "Whoops, wrong url!"
-        }
-        <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <UserContext.Provider value={currentUser}>
+        <UserSelector currentUser={currentUser} setCurrentUser={setCurrentUser} />
+        <header className="App-header">
+          {path === '/coach'
+            ? <CoachPage />
+            : path === '/student'
+              ? <StudentPage />
+              : "Whoops, wrong url!"
+          }
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </UserContext.Provider>
     </div>
   );
 }
