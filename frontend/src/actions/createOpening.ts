748ca8e74bb226d36257d8ad9cdb7426d478bcc6
carmen <carmen@carmens-MacBook-Pro.local>
2024-05-17 00:58:17 +0800

    create opening action

---
+++ frontend/src/actions/createOpening.ts
@@ -0,0 +1,27 @@
+import { Dayjs } from "dayjs";
+
+const createOpening = async (coachId: string, time: Dayjs) => {
+  if (coachId === '') return false;
+  console.log(2, coachId, time)
+  let res;
+  try{
+
+    res = await fetch(`http://localhost:8080/openings`, {
+      method: 'post',
+      headers: {
+        "Content-Type": 'application/json',
+      },
+      body: JSON.stringify({
+        coachId,
+        time: time.toString(),
+      })
+    });
+    if (res.ok) return true;
+  } catch (e) {
+    console.log(e);
+  }
+
+  return false;
+};
+
+export default createOpening;
