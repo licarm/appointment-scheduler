b3a585c81f38fe79a70d1fc19e90b5e33f766fa9
carmen <carmen@carmens-MacBook-Pro.local>
2024-05-17 09:45:57 +0800

    student page skeleton

--- frontend/src/pages/Student.tsx
+++ frontend/src/pages/Student.tsx
@@ -1,8 +1,29 @@
+import { useContext } from "react";
+import { Opening } from "src/Types";
+import TimeSlot, { SlotContainer } from "src/common-components/TimeSlot";
+import useFetchOpenings from "src/hooks/useFetchOpenings";
+import { UserContext } from "src/utils/UserContext";
+
 const StudentPage = () => {
-  return (
-    <>
-    students page
+  const openings = useFetchOpenings();
+  const user = useContext(UserContext);
+
+  return (user
+    ? <>
+      {user.name} page
+      <SlotContainer>
+        {openings.map((o: Opening) => {
+          return <TimeSlot
+            key={o.time + o.user_coach_id}
+            status="slot"
+            onClick={() => {
+              alert('not implemented');
+            }}
+          >{o.time}</TimeSlot>
+        })}
+      </SlotContainer>
     </>
+    : <>select a user</>
   )
 };
 
